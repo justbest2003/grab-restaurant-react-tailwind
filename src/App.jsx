a1ba33df7a0dfe358125fcff0a62dfc7813dfd42
justbest2003 <justbest2003@gmail.com>
2024-06-24 17:11:00 +0700

    json-server

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import Header from "./Components/Header";
 import Search from "./Components/Search";
 import Restaurant from "./Components/Restaurant"; 
-
 import "./App.css";
 
 function App() {
--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Header = () => {
   return (
     <div>
-      <h1 class="font-bold text-3xl p-4 bg-gradient-to-r from-green-400 to-blue-500 text-white shadow-lg rounded-lg text-center">
+      <h1 className="font-bold text-3xl p-4 bg-gradient-to-r from-green-400 to-blue-500 text-white shadow-lg rounded-lg text-center">
   Grab Restaurant
 </h1>
     </div>
