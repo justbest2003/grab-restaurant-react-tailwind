1965b127323b31d6a4856303df76ec76af0e9fb0
justbest2003 <justbest2003@gmail.com>
2024-07-06 10:59:15 +0700

    Change Router

--- src/main.jsx
+++ src/main.jsx
@@ -1,26 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import Home from "./pages/Home.jsx";
-import Add from "./pages/Add.jsx";
-import Edit from "./pages/Edit.jsx";
+import { RouterProvider } from "react-router-dom";
 import Navbar from "./Components/Navbar.jsx";
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Home />,
-  },
-  {
-    path: "/add",
-    element: <Add />,
-  },
-  {
-    path: "/edit/:id",
-    element: <Edit />,
-  }
-]);
+import router from "./Router/Router.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
